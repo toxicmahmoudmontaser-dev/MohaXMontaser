c65c5c492bf900f705dc2cf97bd92148883a1927
toxicmahmoudmontaser-dev <toxicmahmoudmontaser@gmail.com>
2026-06-19 14:01:47 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,1 +1,11 @@
-
+local Games = {
+    {name = "Muscle Legends",     placeId = 3623096087,      url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/muscle%20legends%20.lua"},
+    {name = "Legends of Speed",   placeId = 3101667897,      url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/legends%20of%20speed.lua"},
+    {name = "Ninja Legends",      placeId = 3956818381,      url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/Ninja%20legends%20.lua"},
+    {name = "Murder Mystery 2",   placeId = 142823291,       url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/mm2.lua"},
+    {name = "Hyper Speed Runner", placeId = 88933961678687,  url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/Hyper%20speed%20runner%20.lua"},
+    {name = "Flick",              placeId = 136801880565837, url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/Flick.lua"},
+    {name = "Arsenal",            placeId = 286090429,       url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/Arsenal.lua"},
+    {name = "Tower of Hell",      placeId = 1962086868,      url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/tower%20of%20hell.lua"},
+    {name = "Race Clicker",       placeId = 9285238704,      url = "https://raw.githubusercontent.com/toxicmahmoudmontaser-dev/MohaXMontaser/refs/heads/main/race%20clicker.lua"},
+}
