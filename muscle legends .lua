1edb78c7fc255463a730cd128b11b931cfe2becf
toxicmahmoudmontaser-dev <toxicmahmoudmontaser@gmail.com>
2026-06-19 13:02:39 +0300

    Update muscle legends .lua

--- muscle legends .lua
+++ muscle legends .lua
@@ -1,1 +1,3 @@
+getgenv().SCRIPT_KEY = "6e62d851-69a3-41be-b252-e80250d7cf47"
 
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/ff0d57ce2bcc046b5f041766ef42f6d9349a504569cfff0785da5f73aa8b0892/download"))()
